cc00b4ee107edacda83c82cf754183d8712c85de
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-04-15 14:30:46 +0900

    build時に不要なsassがコンパイルされないように

--- config.rb
+++ config.rb
@@ -47,30 +47,33 @@
 #   end
 # end
 
-set :css_dir, 'css'
+set :css_dir, 'stylesheets'
 
-set :js_dir, 'js'
+set :js_dir, 'javascripts'
 
-set :images_dir, 'image'
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  compass_config do |config|
-    config.output_style = :compressed
-    config.line_comments = false
-  end
+  # compass_config do |config|
+  #   config.output_style = :compressed
+  #   config.line_comments = false
+  # end
   
   activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
 
+  # set :css_dir, '/stylesheets'
+  # set :js_dir, '/javascripts'
+
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
